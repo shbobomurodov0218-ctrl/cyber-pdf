8c3b827eec4f07fe080a59be53b8f4259e4a3045
Shamsiddin Bobomurodov <shbobomurodov0218@gmail.com.>
2026-01-08 22:02:00 +0500

    Render uchun tayyor

--- app.py
+++ app.py
@@ -155,5 +155,7 @@ def temp_file(filename):
 def download_file(filename):
     return send_file(f"temp/{filename}", as_attachment=True)
 
+# ✅ Render.com uchun bitta va to'g'ri `if __name__ == '__main__':`
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
